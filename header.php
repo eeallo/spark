59590850e88cae775cd7cc366d0d23cc03e5906a
Eero L <ealouh@gmail.com>
2015-03-03 14:01:07 +0200

    minor updates

--- header.php
+++ header.php
@@ -4,12 +4,10 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">	
-	
-	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<meta name="description" content="<?php bloginfo( 'description' ); ?>" />
-	
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<?php wp_head(); ?>
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+    <?php wp_head(); ?>
 	<!--[if lt IE 9]>
     <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
