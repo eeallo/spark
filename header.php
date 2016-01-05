b448fa7c0b66b3d6cb64f1c6aca5bec7cb7d1cca
Eero L <ealouh@gmail.com>
2016-01-05 16:23:33 +0200

    duh

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">	
 	<meta name="description" content="<?php bloginfo( 'description' ); ?>" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
